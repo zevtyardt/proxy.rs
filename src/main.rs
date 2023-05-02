85acf215a8c2f38a3103e1d4d1c0e89e38030350
zevtyardt <apklover76@gmail.com>
2023-05-02 19:53:24 +0700

    test resolver

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,36 @@
 use std::net::IpAddr;
 
-use crate::{providers::freeproxylist::FreeProxyListNetProvider, resolver::Resolver};
-mod providers;
+use crate::{judge::Judge, resolver::Resolver};
+mod judge;
 mod resolver;
+mod utils;
 
 fn main() {
-    let mut resolver = Resolver::new();
-    let ip = resolver.get_real_ext_ip().unwrap();
-    let my_ip: Option<IpAddr> = match ip.parse() {
-        Ok(ip) => Some(ip),
-        Err(e) => {
-            println!("{}", e);
-            None
-        }
-    };
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            let resolver = Resolver::new();
+            let ip = resolver.get_real_ext_ip().await.unwrap();
 
-    if let Some(my_ip) = my_ip {
-        let lookup = resolver.get_ip_info(my_ip);
-        println!("{lookup:#?}");
+            let my_ip: IpAddr = ip.parse().unwrap();
+            let lookup = resolver.get_ip_info(my_ip).await;
+            println!("{lookup:#?}");
 
-        let ip = resolver.resolve("google.com");
-        println!("real_ip: {ip:#?}")
-    }
+            for _ in 0..10 {
+                let mut c_resolver = resolver.clone();
+                tokio::task::spawn_blocking(move || {
+                    let ip = c_resolver.resolve("yahoo.com".to_string());
+                    println!("yahoo: {ip:#?}");
+                    let ip = c_resolver.resolve("google.com".to_string());
+                    println!("google: {ip:#?}")
+                })
+                .await
+                .unwrap();
+            }
 
-    let mut p = FreeProxyListNetProvider::new();
-    p.add("tes");
-    println!("{:#?}", p);
-    println!("{}", p.url)
+            let judge = Judge::new("http://httpbin.org/get?show_env");
+            println!("judge: {:#?}", judge.url);
+        })
 }

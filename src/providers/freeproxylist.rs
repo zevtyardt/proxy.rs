69c576e7d52b51480206ee79c703377d0df699fe
zevtyardt <apklover76@gmail.com>
2023-05-11 09:11:37 +0700

    feat: added free-proxy-list.net proxy scraper

--- src/providers/freeproxylist.rs
+++ src/providers/freeproxylist.rs
@@ -1,20 +1,31 @@
 use super::base_provider::BaseProvider;
+use crate::proxy::Proxy;
+use regex::Regex;
 
 #[derive(Debug)]
-pub struct FreeProxyListNetProvider {
-    pub base_provider: BaseProvider,
-    pub url: String,
+pub struct FreeProxyListNetProvider<'a> {
+    pub base: BaseProvider<'a>,
+    pub url: &'a str,
+    pub pattern: &'a str,
 }
 
-impl FreeProxyListNetProvider {
-    pub fn new() -> Self {
-        FreeProxyListNetProvider {
-            base_provider: BaseProvider::new(),
-            url: "https://www.freeproxylists.net".to_string(),
-        }
+impl FreeProxyListNetProvider<'_> {
+    pub async fn get_proxies(&self) -> Vec<(String, u16, Vec<&str>)> {
+        let req = self.base.client.get(self.url);
+        let html = self.base.get_html(req).await;
+        self.base.find_proxies(self.pattern, html.as_str())
     }
+}
 
-    pub fn add(&mut self, proxy: &str) {
-        self.base_provider.proxies.push(proxy.to_string())
+impl Default for FreeProxyListNetProvider<'_> {
+    fn default() -> Self {
+        let mut base_provider = BaseProvider::default();
+        base_provider.proto = vec!["HTTP", "CONNECT:80", "HTTPS", "CONNECT:25"];
+
+        FreeProxyListNetProvider {
+            base: base_provider,
+            url: "https://free-proxy-list.net/",
+            pattern: r#"(?P<ip>(?:\d+\.?){4})\:(?P<port>\d+)"#,
+        }
     }
 }

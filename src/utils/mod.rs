4cfd24a1443044db5e5b8580bda90f440d1c7f19
zevtyardt <apklover76@gmail.com>
2023-05-11 18:55:35 +0700

    feat(utils): Add vec_of_strings macro
    - Add `vec_of_strings` macro to easily create a vector of strings

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -13,3 +13,9 @@ pub async fn run_parallel<T>(tasks: Vec<CustomFuture<T>>, num_concurrent: usize)
         .collect::<Vec<T>>()
         .await
 }
+
+macro_rules! vec_of_strings {
+    ($($x:expr),*) => (vec![$($x.to_string()),*]);
+}
+
+pub(crate) use vec_of_strings;

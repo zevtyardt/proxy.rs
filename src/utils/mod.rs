96a12aff164d00a88f65ef5eb1cb82011661f416
zevtyardt <apklover76@gmail.com>
2023-05-11 21:17:13 +0700

    refactor(utils): Remove unused code and run_parallel function
    - Remove unused imports and type alias
    - Remove run_parallel function as it's no longer used

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,18 +1,6 @@
-use futures_util::{stream, StreamExt};
-
 pub mod geolite_database;
 pub mod http;
 pub mod queue;
-
-pub type CustomFuture<T> = std::pin::Pin<Box<dyn std::future::Future<Output = T>>>;
-
-/* Additional functions to run future tasks in parallel */
-pub async fn run_parallel<T>(tasks: Vec<CustomFuture<T>>, num_concurrent: usize) -> Vec<T> {
-    stream::iter(tasks)
-        .buffer_unordered(num_concurrent)
-        .collect::<Vec<T>>()
-        .await
-}
 
 macro_rules! vec_of_strings {
     ($($x:expr),*) => (vec![$($x.to_string()),*]);

94580d1fff003699c8940bc20a1266e35a2cb2e8
zevtyardt <apklover76@gmail.com>
2023-05-11 18:56:49 +0700

    refactor(freeproxylist): Refactor FreeProxyListNetProvider struct
    - Change the type of `url` and `pattern` fields from string literals to String type
    - Update the return type of `get_proxies` method to use String instead of &str
    - Use the `vec_of_strings` macro from utils module
    - Update default values for `proto` and `domain` fields

--- src/providers/freeproxylist.rs
+++ src/providers/freeproxylist.rs
@@ -1,31 +1,37 @@
 use super::base_provider::BaseProvider;
-use crate::proxy::Proxy;
+use crate::{proxy::Proxy, utils::vec_of_strings};
 use regex::Regex;
 
 #[derive(Debug)]
-pub struct FreeProxyListNetProvider<'a> {
-    pub base: BaseProvider<'a>,
-    pub url: &'a str,
-    pub pattern: &'a str,
+pub struct FreeProxyListNetProvider {
+    pub base: BaseProvider,
+    pub url: String,
+    pub pattern: String,
 }
 
-impl FreeProxyListNetProvider<'_> {
-    pub async fn get_proxies(&self) -> Vec<(String, u16, Vec<&str>)> {
-        let req = self.base.client.get(self.url);
+impl FreeProxyListNetProvider {
+    pub async fn get_proxies(&mut self) -> Vec<(String, u16, Vec<String>)> {
+        self.base.start();
+
+        let req = self.base.client.get(self.url.clone());
         let html = self.base.get_html(req).await;
-        self.base.find_proxies(self.pattern, html.as_str())
+        let proxies = self.base.find_proxies(self.pattern.clone(), html.as_str());
+        self.base.update_stack(&proxies).await;
+
+        proxies
     }
 }
 
-impl Default for FreeProxyListNetProvider<'_> {
+impl Default for FreeProxyListNetProvider {
     fn default() -> Self {
         let mut base_provider = BaseProvider::default();
-        base_provider.proto = vec!["HTTP", "CONNECT:80", "HTTPS", "CONNECT:25"];
+        base_provider.proto = vec_of_strings!["HTTP", "CONNECT:80", "HTTPS", "CONNECT:25"];
+        base_provider.domain = "free-proxy-list.net".to_string();
 
         FreeProxyListNetProvider {
             base: base_provider,
-            url: "https://free-proxy-list.net/",
-            pattern: r#"(?P<ip>(?:\d+\.?){4})\:(?P<port>\d+)"#,
+            url: "https://free-proxy-list.net/".to_string(),
+            pattern: r#"(?P<ip>(?:\d+\.?){4})\:(?P<port>\d+)"#.to_string(),
         }
     }
 }

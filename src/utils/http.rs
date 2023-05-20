f0b47609f57cbbaf39cc48fdf2d220f967441110
zevtyardt <apklover76@gmail.com>
2023-05-21 02:06:59 +0700

    refactor(utils): Improve random_useragent function
    fix(utils): Fix header name case in Response struct
    
    feat(utils): Add get_headers function

--- src/utils/http.rs
+++ src/utils/http.rs
@@ -4,13 +4,36 @@ use autocompress::Decoder;
 use httparse::EMPTY_HEADER;
 use rand::Rng;
 
-pub fn random_useragent() -> String {
-    let mut rng = rand::thread_rng();
+pub fn random_useragent(random_value: bool) -> String {
     let name = option_env!("CARGO_PKG_NAME").unwrap_or("proxy-rs");
     let version = option_env!("CARGO_PKG_VERSION").unwrap_or("0.0.0");
-    let rv = rng.gen_range(1000..9999);
 
-    format!("{}/{}/{}", name, version, rv)
+    let mut rv = "".to_string();
+    if random_value {
+        let mut rng = rand::thread_rng();
+        rv.push('/');
+        rv.push_str(rng.gen_range(1000..9999).to_string().as_str())
+    }
+
+    format!("{}/{}{}", name, version, rv)
+}
+
+pub fn get_headers(random_value: bool) -> (HashMap<String, String>, String) {
+    let ua = random_useragent(random_value);
+
+    let ua_c = ua.clone();
+    let rv = ua_c.split("/").last().unwrap();
+    let mut headers = HashMap::new();
+
+    headers.insert("User-Agent".to_string(), ua);
+    headers.insert("Accept".to_string(), "*/*".to_string());
+    headers.insert("Accept-Encoding".to_string(), "gzip, deflate".to_string());
+    headers.insert("Pragma".to_string(), "no-cache".to_string());
+    headers.insert("Cache-Control".to_string(), "no-cache".to_string());
+    headers.insert("Cookie".to_string(), "cookie=ok".to_string());
+    headers.insert("Referer".to_string(), "https://google.com/".to_string());
+
+    (headers, rv.to_string())
 }
 
 #[derive(Debug)]
@@ -20,6 +43,7 @@ pub struct Response {
     pub reason: Option<String>,
     pub headers: HashMap<String, String>,
     pub body: String,
+    pub raw: String,
 }
 
 impl Response {
@@ -28,6 +52,7 @@ impl Response {
         let mut chunk = vec![EMPTY_HEADER; 128];
         let mut parser = httparse::Response::new(&mut chunk);
 
+        response.raw = String::from_utf8_lossy(&data).to_string();
         if let Ok(httparse::Status::Complete(n)) = parser.parse(data) {
             response.version = parser.version;
             response.status_code = parser.code;
@@ -35,7 +60,7 @@ impl Response {
 
             for header in parser.headers {
                 response.headers.insert(
-                    header.name.to_string(),
+                    header.name.to_string().to_lowercase(),
                     String::from_utf8_lossy(header.value).to_string(),
                 );
             }
@@ -69,6 +94,7 @@ impl Default for Response {
             reason: None,
             headers: HashMap::new(),
             body: String::new(),
+            raw: String::new(),
         }
     }
 }

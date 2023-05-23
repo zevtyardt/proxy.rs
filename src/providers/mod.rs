a7209714e5d065c0153ba2d2ba777e449740b231
zevtyardt <apklover76@gmail.com>
2023-05-23 18:24:19 +0700

    feat(providers): Add Proxyscan providers
    - Import the necessary modules for the Proxyscan providers in the providers module.
    - Add the Proxyscan providers (HTTP, HTTPS, SOCKS4, SOCKS5).

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -2,53 +2,82 @@ pub mod base_provider;
 pub mod freeproxylist;
 pub mod git_repo;
 pub mod ipaddress_com;
+pub mod proxyscan;
 pub mod proxyscrape;
 
-use lazy_static::lazy_static;
-use tokio::task::{self, JoinHandle};
+use std::sync::{Arc, Mutex};
 
-use crate::{proxy::Proxy, utils::queue::FifoQueue};
+use concurrent_queue::ConcurrentQueue;
+use lazy_static::lazy_static;
+use tokio::{spawn, task::JoinHandle};
+
+use crate::proxy::Proxy;
+
 lazy_static! {
-    pub static ref PROXIES: FifoQueue<Proxy> = FifoQueue::new();
+    pub static ref PROXIES: ConcurrentQueue<Proxy> = ConcurrentQueue::unbounded();
+    pub static ref UNIQUE_PROXIES: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(vec![]));
 }
 
 pub fn get_all_tasks() -> Vec<JoinHandle<()>> {
     let mut tasks = vec![];
 
     /* ===== */
-    tasks.push(task::spawn(async {
+    tasks.push(spawn(async {
         let mut freeproxylist = freeproxylist::FreeProxyListNetProvider::default();
         freeproxylist.get_proxies().await;
     }));
 
     /* ===== */
-    tasks.push(task::spawn(async {
+    tasks.push(spawn(async {
         let mut ipaddress_com = ipaddress_com::IpaddressComProvider::default();
         ipaddress_com.get_proxies().await;
     }));
 
     /* ===== */
-    tasks.push(task::spawn(async {
+    tasks.push(spawn(async {
         let mut proxyscrape_http =
             proxyscrape::proxyscrape_http::ProxyscrapeComHttpProvider::default();
         proxyscrape_http.get_proxies().await;
     }));
-    tasks.push(task::spawn(async {
+    tasks.push(spawn(async {
         let mut proxyscrape_socks4 =
             proxyscrape::proxyscrape_socks4::ProxyscrapeComSocks4Provider::default();
         proxyscrape_socks4.get_proxies().await;
     }));
-    tasks.push(task::spawn(async {
+    tasks.push(spawn(async {
         let mut proxyscrape_socks5 =
             proxyscrape::proxyscrape_socks5::ProxyscrapeComSocks5Provider::default();
         proxyscrape_socks5.get_proxies().await;
     }));
 
     /* ===== */
-    tasks.push(task::spawn(async {
+    tasks.push(spawn(async {
         let mut zevtyardt_proxy_list =
             git_repo::zevtyardt_proxy_list::GithubZevtyardtProxyListProvider::default();
         zevtyardt_proxy_list.get_proxies().await;
+    }));
+
+    /* ===== */
+    tasks.push(spawn(async {
+        let mut proxyscan_http = proxyscan::proxyscan_http::ProxyscanIoHttpProvider::default();
+        proxyscan_http.get_proxies().await;
+    }));
+
+    tasks.push(spawn(async {
+        let mut proxyscan_https = proxyscan::proxyscan_https::ProxyscanIoHttpsProvider::default();
+        proxyscan_https.get_proxies().await;
+    }));
+
+    tasks.push(spawn(async {
+        let mut proxyscan_socks4 =
+            proxyscan::proxyscan_socks4::ProxyscanIoSocks4Provider::default();
+        proxyscan_socks4.get_proxies().await;
+    }));
+
+    tasks.push(spawn(async {
+        let mut proxyscan_socks5 =
+            proxyscan::proxyscan_socks5::ProxyscanIoSocks5Provider::default();
+        proxyscan_socks5.get_proxies().await;
     }));
 
     tasks

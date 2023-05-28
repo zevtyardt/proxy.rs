e4751f07b7f263e580523404e03e5ca80e815172
zevtyardt <apklover76@gmail.com>
2023-05-28 21:13:27 +0700

    refactor(checker): Change collections type from HashMap to BTreeMap, add negotiate function, and update build_raw_request function
    - Add a new function 'negotiate' to handle proxy negotiation based on the protocol
    - Update the 'build_raw_request' function to include an additional parameter 'use_full_path' to determine whether to use the full path in the request
    - Update variable names and log statements for clarity

--- src/checker.rs
+++ src/checker.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, process::exit, sync::Arc};
+use std::{collections::BTreeMap, process::exit, sync::Arc};
 
 use parking_lot::{Condvar, Mutex};
 use rand::seq::SliceRandom;
@@ -7,7 +7,10 @@ use tokio::{spawn, time};
 
 use crate::{
     judge::{get_judges, Judge},
-    negotiators::http_negotiator::HttpNegotiator,
+    negotiators::{
+        http_negotiator::HttpNegotiator, https_negotiator::HttpsNegotiator,
+        socks4_negotiator::Socks4Negotiator,
+    },
     proxy::Proxy,
     resolver::Resolver,
     utils::{
@@ -24,7 +27,7 @@ pub struct Checker {
     pub method: String,
 
     disable_protocols: Arc<Mutex<Vec<String>>>,
-    judges: Arc<Mutex<HashMap<String, Vec<Judge>>>>,
+    judges: Arc<Mutex<BTreeMap<String, Vec<Judge>>>>,
     ext_ip: String,
     ip_re: Regex,
     cv: Arc<Condvar>,
@@ -50,6 +53,11 @@ impl Checker {
 
         let mut working = 0;
         for judge in all_judges {
+            if judge.is_none() {
+                continue;
+            }
+
+            let judge = judge.unwrap();
             if judge.is_working {
                 let mut judges_by_scheme = self.judges.lock();
                 let scheme = &judge.scheme;
@@ -94,13 +102,11 @@ impl Checker {
     }
 
     pub async fn check_proxy(&mut self, proxy: &mut Proxy) {
-        let expected_types = proxy.expected_types.clone();
+        let expected_types = vec_of_strings!["HTTP", "HTTPS"]; //proxy.expected_types.clone();
 
         let mut result = vec![];
-        for proto in expected_types {
-            if proto == "HTTP" {
-                result.push(self.check_proto(proxy, &proto).await);
-            }
+        for proto in &expected_types {
+            result.push(self.check_proto(proxy, proto).await);
         }
 
         if result.iter().any(|i| *i) {
@@ -113,40 +119,44 @@ impl Checker {
             return false;
         }
 
-        let judge = self.get_judge(proto);
-        log::debug!("Selected judge: {}", judge);
         proxy.negotiator_proto = proto.to_string();
-        if !proxy.connect().await {
+        let judge = self.get_judge(proto);
+        proxy.log(format!("Selected judge: {}", judge).as_str(), None, None);
+
+        if proto != "HTTPS" && !proxy.connect().await {
             return false;
         }
-        let negotiator = HttpNegotiator::default();
-        if !negotiator
-            .negotiate(&judge.host, &judge.ip_address.unwrap())
-            .await
-        {
+
+        let (negotiate_success, use_full_path, check_anon_lvl) =
+            self.negotiate(proxy, &judge, proto).await;
+        if !negotiate_success {
             return false;
         }
-        let (raw_request, headers, rv) = self.build_raw_request(
-            &judge.scheme.to_lowercase(),
-            &judge.host,
-            &judge.url.path().to_string(),
-            None,
-        );
+
+        let path = judge.url.path().to_string();
+        let (raw_request, headers, rv) =
+            self.build_raw_request(&judge.host, &path, use_full_path, None);
         let mut is_working = false;
+
         proxy.send(raw_request.as_bytes()).await;
         if let Some(data) = proxy.recv_all().await {
             proxy.log("Request: success", None, None);
+            let mut anonimity_lvl = None;
             let response = Response::parse(data.as_slice());
 
-            if self.is_response_correct(&response, headers, rv) {
-                let anonimity_lvl = self.get_anonimity_level(&response, judge.marks);
-                proxy.types.push((proto.to_string(), Some(anonimity_lvl)));
+            // log::warn!("=====\n{raw_request}\n{0}\n{1}", response.raw, proto);
+
+            if self.is_response_correct(&response, headers, rv, proxy) {
+                if check_anon_lvl {
+                    anonimity_lvl = Some(self.get_anonimity_level(&response, judge.marks));
+                }
+
                 is_working = true;
+                proxy.types.push((proto.to_string(), anonimity_lvl));
             }
             proxy.close().await;
         } else {
             proxy.log("Request: failed", None, Some("request_failed".to_string()));
-            is_working = false;
         }
         is_working
     }
@@ -159,7 +169,37 @@ impl Checker {
         unimplemented!();
     }
 
-    fn get_anonimity_level(&self, response: &Response, marks: HashMap<String, usize>) -> String {
+    async fn negotiate(
+        &self,
+        proxy: &mut Proxy,
+        judge: &Judge,
+        proto: &String,
+    ) -> (bool, bool, bool) {
+        if proto == "SOCKS4" {
+            let negotiator = Socks4Negotiator::default();
+            (
+                negotiator.negotiate(proxy, judge).await,
+                negotiator.use_full_path,
+                negotiator.check_anon_lvl,
+            )
+        } else if proto == "HTTPS" {
+            let negotiator = HttpsNegotiator::default();
+            (
+                negotiator.negotiate(proxy, judge).await,
+                negotiator.use_full_path,
+                negotiator.check_anon_lvl,
+            )
+        } else {
+            let negotiator = HttpNegotiator::default();
+            (
+                negotiator.negotiate().await,
+                negotiator.use_full_path,
+                negotiator.check_anon_lvl,
+            )
+        }
+    }
+
+    fn get_anonimity_level(&self, response: &Response, marks: BTreeMap<String, usize>) -> String {
         let content = response.body.to_lowercase();
         let mut via = false;
         if let Some(via_m) = marks.get("via") {
@@ -189,34 +229,43 @@ impl Checker {
     fn is_response_correct(
         &self,
         response: &Response,
-        headers: HashMap<String, String>,
+        headers: BTreeMap<String, String>,
         rv: String,
+
+        proxy: &mut Proxy,
     ) -> bool {
-        let response_raw = &response.raw;
+        let response_raw = response.raw.to_lowercase();
         let version_is_correct = response_raw.contains(&rv);
         let support_referer = if let Some(referer) = headers.get("Referer") {
-            response_raw.contains(referer)
+            response_raw.contains(&referer.to_lowercase())
         } else {
             false
         };
         let support_cookie = if let Some(cookie) = headers.get("Cookie") {
-            response_raw.contains(cookie)
+            response_raw.contains(&cookie.to_lowercase())
         } else {
             false
         };
-        let some_ip = self.ip_re.find(response_raw);
+        let some_ip = self.ip_re.find(&response_raw).is_some();
         let is_ok = response.status_code.unwrap_or(0) == 200;
-        is_ok && version_is_correct && support_referer && support_cookie && some_ip.is_some()
+
+        //println!("{response:#?}: is_ok: {is_ok}, some_ip: {some_ip}, support_cookie: {support_cookie}, support_referer: {support_referer}, rv: {version_is_correct}, {proxy}");
+        is_ok && version_is_correct && some_ip && support_referer && support_cookie
     }
 
     fn build_raw_request(
         &self,
-        scheme: &String,
         host: &String,
         path: &String,
+        use_full_path: bool,
         data: Option<String>,
-    ) -> (String, HashMap<String, String>, String) {
-        let mut request = format!("{} {}://{}{} HTTP/1.1\r\n", self.method, scheme, host, path);
+    ) -> (String, BTreeMap<String, String>, String) {
+        let mut request = if use_full_path {
+            format!("{} http://{}{} HTTP/1.1\r\n", self.method, host, path)
+        } else {
+            format!("{} {} HTTP/1.1\r\n", self.method, path)
+        };
+
         let (mut headers, rv) = get_headers(true);
         let data = data.unwrap_or("".to_string());
         headers.insert("Host".to_string(), host.to_string());
@@ -233,6 +282,7 @@ impl Checker {
         }
         request += "\r\n";
         request += data.as_str();
+
         (request, headers, rv)
     }
 
@@ -263,7 +313,7 @@ impl Checker {
             timeout: 5,
             max_tries: 3,
             method: "GET".to_string(),
-            judges: Arc::new(Mutex::new(HashMap::new())),
+            judges: Arc::new(Mutex::new(BTreeMap::new())),
             disable_protocols: Arc::new(Mutex::new(vec![])),
             ip_re: Regex::new(r#"\d+\.\d+\.\d+\.\d+"#).unwrap(),
             ext_ip: resolver.get_real_ext_ip().await.unwrap(),

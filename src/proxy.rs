281a3b3f9dbd7526b2a6e78ca9d03a8d3bc72975
zevtyardt <apklover76@gmail.com>
2023-05-28 21:02:05 +0700

    feat(proxy): Added tls streams to the proxy

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,12 +1,17 @@
-use std::{collections::HashMap, net::IpAddr, time::Duration};
+use std::{collections::HashMap, net::IpAddr, sync::Arc, time::Duration};
 
+use native_tls::TlsConnector;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
     time::{timeout, Instant},
 };
+use tokio_native_tls::TlsStream;
 
-use crate::resolver::{GeoData, Resolver};
+use crate::{
+    resolver::{GeoData, Resolver},
+    utils::http::Response,
+};
 
 #[derive(Debug)]
 pub struct Proxy {
@@ -17,10 +22,14 @@ pub struct Proxy {
     pub types: Vec<(String, Option<String>)>,
     pub logs: Vec<(String, String, Duration)>,
     pub negotiator_proto: String,
+
+    pub verify_ssl: bool,
     pub timeout: i32,
     pub runtimes: Vec<f64>,
 
-    pub stream: Option<TcpStream>,
+    pub tcp_stream: Option<TcpStream>,
+    pub tls_stream: Option<TlsStream<TcpStream>>,
+
     pub request_stat: i32,
     pub error_stat: HashMap<String, i32>,
 }
@@ -45,7 +54,9 @@ impl Proxy {
                 negotiator_proto: "HTTP".to_string(),
                 timeout: 5,
                 runtimes: vec![],
-                stream: None,
+                tcp_stream: None,
+                tls_stream: None,
+                verify_ssl: false,
                 request_stat: 0,
                 error_stat: HashMap::new(),
             });
@@ -102,9 +113,63 @@ impl Proxy {
     }
 
     pub async fn connect(&mut self) -> bool {
+        self.tcp_stream = self.connect_tcp().await;
+        self.tcp_stream.is_some()
+    }
+
+    pub async fn send(&mut self, body: &[u8]) {
+        if self.tls_stream.is_some() {
+            self.send_tls(&body).await;
+        } else if self.tcp_stream.is_some() {
+            self.send_tcp(&body).await
+        } else {
+            log::error!("must connect to the proxy first");
+        }
+    }
+
+    pub async fn recv(&mut self, size: usize) -> Option<Vec<u8>> {
+        if self.tls_stream.is_some() {
+            return self.recv_tls(size).await;
+        } else if self.tcp_stream.is_some() {
+            return self.recv_tcp(size).await;
+        } else {
+            log::error!("must connect to the proxy first");
+        }
+        None
+    }
+
+    pub async fn recv_all(&mut self) -> Option<Vec<u8>> {
+        if self.tls_stream.is_some() {
+            return self.recv_all_tls().await;
+        } else if self.tcp_stream.is_some() {
+            return self.recv_all_tcp().await;
+        }
+        None
+    }
+
+    pub async fn close(&mut self) {
+        if let Some(stream) = self.tcp_stream.as_mut() {
+            match stream.shutdown().await {
+                Ok(_) => {
+                    self.tcp_stream = None;
+                    self.log("Connection closed", None, None)
+                }
+                Err(e) => self.log(
+                    format!("Failed to close connection: {}", e).as_str(),
+                    None,
+                    Some(e.to_string()),
+                ),
+            }
+        }
+    }
+}
+
+// TCP
+impl Proxy {
+    async fn connect_tcp(&mut self) -> Option<TcpStream> {
         let stime = Instant::now();
         self.log("Initial connection", Some(stime.elapsed()), None);
-        self.stream = match timeout(
+        match timeout(
             Duration::from_secs(self.timeout as u64),
             TcpStream::connect(self.as_text()),
         )
@@ -126,29 +191,15 @@ impl Proxy {
                 }
             },
             Err(e) => {
-                self.log(
-                    "Connection timeout",
-                    Some(stime.elapsed()),
-                    Some(e.to_string()),
-                );
+                self.log("Connection timeout", None, Some(e.to_string()));
                 None
             }
-        };
-
-        self.stream.is_some()
-    }
-
-    pub fn connect_ssl(&self) {
-        unimplemented!()
-    }
-
-    pub async fn send(&mut self, body: &[u8]) {
-        if self.stream.is_none() {
-            log::debug!("Please run .connect() first!");
-            return;
         }
+    }
+
+    async fn send_tcp(&mut self, body: &[u8]) {
         let stime = Instant::now();
-        let stream = self.stream.as_mut().unwrap();
+        let stream = self.tcp_stream.as_mut().unwrap();
         match stream.write_all(body).await {
             Ok(_) => self.log(
                 format!("Sending {} bytes", body.len()).as_str(),
@@ -165,14 +216,45 @@ impl Proxy {
         };
     }
 
-    pub async fn recv_all(&mut self) -> Option<Vec<u8>> {
-        if self.stream.is_none() {
-            log::debug!("Please run .connect() first");
-            return None;
-        }
-
+    async fn recv_tcp(&mut self, size: usize) -> Option<Vec<u8>> {
         let stime = Instant::now();
-        let stream = self.stream.as_mut().unwrap();
+        let stream = self.tcp_stream.as_mut().unwrap();
+        let mut chunk = vec![0; size];
+        match timeout(
+            Duration::from_secs(self.timeout as u64),
+            stream.read_exact(&mut chunk),
+        )
+        .await
+        {
+            Ok(buffer) => match buffer {
+                Ok(buf_size) => {
+                    if buf_size > 0 {
+                        self.log(
+                            format!("Received {} bytes", buf_size).as_str(),
+                            Some(stime.elapsed()),
+                            None,
+                        );
+                        return Some(chunk.to_vec());
+                    }
+                }
+                Err(e) => self.log(
+                    format!("Failed to receive: {}", e).as_str(),
+                    Some(stime.elapsed()),
+                    Some(e.to_string()),
+                ),
+            },
+            Err(e) => self.log(
+                format!("Received timeout: {}", e).as_str(),
+                None,
+                Some(e.to_string()),
+            ),
+        }
+        None
+    }
+
+    async fn recv_all_tcp(&mut self) -> Option<Vec<u8>> {
+        let stime = Instant::now();
+        let stream = self.tcp_stream.as_mut().unwrap();
         let mut chunk = vec![0; 1024];
         let mut buf = Vec::new();
         loop {
@@ -191,7 +273,11 @@ impl Proxy {
                         buf.extend(data)
                     }
                     Err(e) => {
-                        log::debug!("{}", e);
+                        self.log(
+                            format!("Failed to receive: {}", e).as_str(),
+                            Some(stime.elapsed()),
+                            Some(e.to_string()),
+                        );
                         break;
                     }
                 },
@@ -199,7 +285,7 @@ impl Proxy {
                 Err(e) => {
                     self.log(
                         format!("Received timeout: {}", e).as_str(),
-                        Some(stime.elapsed()),
+                        None,
                         Some(e.to_string()),
                     );
                     // TODO: log error
@@ -214,25 +300,229 @@ impl Proxy {
         );
         Some(buf)
     }
+}
 
-    pub async fn close(&mut self) {
-        if let Some(stream) = self.stream.as_mut() {
-            match stream.shutdown().await {
-                Ok(_) => {
-                    self.stream = None;
-                    self.log("Connection closed", None, None)
+// TLS / SSL
+impl Proxy {
+    /// Only used to check the https protocol not for servers.
+    pub async fn connect_ssl(&mut self, connect_payload: &[u8]) -> bool {
+        if self.tcp_stream.is_some() {
+            self.logs.clear();
+            self.runtimes.clear();
+            self.request_stat = 0;
+        }
+
+        let tcp_stream = self.connect_tcp().await;
+        if tcp_stream.is_none() {
+            return false;
+        }
+
+        let mut tcp_stream = tcp_stream.unwrap();
+
+        // send connnect requests
+        let stime_send = Instant::now();
+        match tcp_stream.write_all(connect_payload).await {
+            Ok(_) => self.log(
+                format!("Sending {} bytes", connect_payload.len()).as_str(),
+                Some(stime_send.elapsed()),
+                None,
+            ),
+            Err(e) => {
+                self.log(
+                    format!("Sending error: {}", e).as_str(),
+                    Some(stime_send.elapsed()),
+                    Some(e.to_string()),
+                );
+                return false;
+            }
+        };
+
+        // recv response
+        let stime_recv = Instant::now();
+        let mut chunk = vec![0; 2048];
+        match timeout(
+            Duration::from_secs(self.timeout as u64),
+            tcp_stream.read(&mut chunk),
+        )
+        .await
+        {
+            Ok(buffer) => match buffer {
+                Ok(buf_size) => {
+                    if buf_size > 0 {
+                        self.log(
+                            format!("Received {} bytes", buf_size).as_str(),
+                            Some(stime_recv.elapsed()),
+                            None,
+                        );
+                    }
                 }
-                Err(e) => self.log(
-                    format!("Failed to close connection: {}", e).as_str(),
+                Err(e) => {
+                    self.log(
+                        format!("Failed to receive: {}", e).as_str(),
+                        Some(stime_recv.elapsed()),
+                        Some(e.to_string()),
+                    );
+                    return false;
+                }
+            },
+            Err(e) => {
+                self.log(
+                    format!("Received timeout: {}", e).as_str(),
                     None,
                     Some(e.to_string()),
-                ),
+                );
+                return false;
             }
         }
+
+        let response = Response::parse(&chunk);
+        if response.status_code.unwrap_or(0) != 200 {
+            return false;
+        }
+
+        let stime = Instant::now();
+        self.log("SSL: Initial connection", Some(stime.elapsed()), None);
+
+        let config = TlsConnector::builder()
+            .danger_accept_invalid_certs(!self.verify_ssl)
+            .build()
+            .unwrap();
+        let connector = tokio_native_tls::TlsConnector::from(config);
+        self.tls_stream = match timeout(
+            Duration::from_secs(self.timeout as u64),
+            connector.connect(&self.host, tcp_stream),
+        )
+        .await
+        {
+            Ok(stream) => match stream {
+                Ok(stream) => {
+                    self.log("SSL: Connection success", Some(stime.elapsed()), None);
+                    self.request_stat += 1;
+                    Some(stream)
+                }
+                Err(e) => {
+                    self.log(
+                        format!("SSL: Connection error: {}", e).as_str(),
+                        Some(stime.elapsed()),
+                        Some(e.to_string()),
+                    );
+                    None
+                }
+            },
+            Err(e) => {
+                self.log("SSL: Connection timeout", None, Some(e.to_string()));
+                None
+            }
+        };
+
+        self.tls_stream.is_some()
+    }
+
+    async fn send_tls(&mut self, body: &[u8]) {
+        let stime = Instant::now();
+        let stream = self.tls_stream.as_mut().unwrap();
+        match stream.write_all(body).await {
+            Ok(_) => self.log(
+                format!("SSL: Sending {} bytes", body.len()).as_str(),
+                Some(stime.elapsed()),
+                None,
+            ),
+            Err(e) => {
+                self.log(
+                    format!("SSL: Sending error: {}", e).as_str(),
+                    Some(stime.elapsed()),
+                    Some(e.to_string()),
+                );
+            }
+        };
+    }
+
+    async fn recv_tls(&mut self, size: usize) -> Option<Vec<u8>> {
+        let stime = Instant::now();
+        let stream = self.tls_stream.as_mut().unwrap();
+        let mut chunk = vec![0; size];
+        match timeout(
+            Duration::from_secs(self.timeout as u64),
+            stream.read_exact(&mut chunk),
+        )
+        .await
+        {
+            Ok(buffer) => match buffer {
+                Ok(buf_size) => {
+                    if buf_size > 0 {
+                        self.log(
+                            format!("SSL: Received {} bytes", buf_size).as_str(),
+                            Some(stime.elapsed()),
+                            None,
+                        );
+                        return Some(chunk.to_vec());
+                    }
+                }
+                Err(e) => self.log(
+                    format!("SSL: Failed to receive: {}", e).as_str(),
+                    Some(stime.elapsed()),
+                    Some(e.to_string()),
+                ),
+            },
+            Err(e) => self.log(
+                format!("SSL: Received timeout: {}", e).as_str(),
+                None,
+                Some(e.to_string()),
+            ),
+        }
+        None
+    }
+
+    async fn recv_all_tls(&mut self) -> Option<Vec<u8>> {
+        let stime = Instant::now();
+        let stream = self.tls_stream.as_mut().unwrap();
+        let mut chunk = vec![0; 512];
+        let mut buf = Vec::new();
+        loop {
+            match timeout(
+                Duration::from_secs(self.timeout as u64),
+                stream.read(&mut chunk),
+            )
+            .await
+            {
+                Ok(buffer) => match buffer {
+                    Ok(buf_size) => {
+                        if buf_size == 0 {
+                            break;
+                        }
+                        let data = &chunk[0..buf_size];
+                        buf.extend(data)
+                    }
+                    Err(e) => {
+                        self.log(
+                            format!("SSL: Failed to receive: {}", e).as_str(),
+                            Some(stime.elapsed()),
+                            Some(e.to_string()),
+                        );
+                        break;
+                    }
+                },
+
+                Err(e) => {
+                    self.log(
+                        format!("SSL: Received timeout: {}", e).as_str(),
+                        None,
+                        Some(e.to_string()),
+                    );
+                    // TODO: log error
+                    break;
+                }
+            }
+        }
+        self.log(
+            format!("SSL: Received {} bytes", buf.len()).as_str(),
+            Some(stime.elapsed()),
+            None,
+        );
+        Some(buf)
     }
 }
 
-// TODO ADD TYPES
 impl std::fmt::Display for Proxy {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut types = vec![];

952b544ff42acf9703b8893a5d488c36b84e87fe
zevtyardt <apklover76@gmail.com>
2023-06-07 12:57:29 +0700

    feat(argument): Update CLI argument options
    - Change the default value of max_conn to 50 for better resource management.
    - Update the default value of max_tries to 1.
    - Increase the timeout default value to 8 seconds.
    - Add a new log_level option for setting the logging level (debug, info, warn, error).
    - Refactor the CLI structure to include subcommands and their respective arguments.
    - Add a new subcommand 'Grab' with its arguments for finding proxies without checks.
    - Add a new subcommand 'Find' with its arguments for finding and checking proxies.

--- src/argument.rs
+++ src/argument.rs
@@ -1,21 +1,103 @@
-use clap::Parser;
+use clap::builder::PossibleValue;
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser, Debug, Clone)]
-#[command(about)]
 pub struct Cli {
-    ///The maximum number of concurrent checks of proxies
-    #[arg(long, default_value = "200")]
+    /// The maximum number of concurrent checks of proxies
+    #[arg(long, default_value = "50")]
     pub max_conn: usize,
 
-    ///The maximum number of attempts to check a proxy
+    /// The maximum number of attempts to check a proxy
     #[arg(long, default_value = "1")]
     pub max_tries: usize,
 
-    ///Time in seconds before giving up
+    /// Time in seconds before giving up
     #[arg(short, long, default_value = "8")]
     pub timeout: usize,
 
-    ///The maximum number of working proxies
+    /// Logging level
+    #[arg(long = "log", default_value = "warn", 
+        value_parser([
+            PossibleValue::new("debug"),
+            PossibleValue::new("info"),
+            PossibleValue::new("warn"),
+            PossibleValue::new("error")
+        ])
+    )]
+    pub log_level: String,
+
+    #[command(subcommand)]
+    pub sub: Commands,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+pub enum Commands {
+    /// Find proxies without a checks
+    Grab(GrabArgs),
+
+    /// Find and check proxies
+    Find(FindArgs),
+}
+
+#[derive(Args, Debug, Clone)]
+pub struct GrabArgs {
+    /// List of ISO country codes where should be located proxies
+    #[arg(short, long, num_args(1..))]
+    pub countries: Vec<String>,
+
+    /// The maximum number of working proxies
     #[arg(short, long)]
     pub limit: Option<usize>,
+
+    /// Flag indicating in what format the results will be presented.
+    #[arg(short, long,
+        default_value = "default",
+        value_parser([
+            PossibleValue::new("default"),
+            PossibleValue::new("text"),
+            PossibleValue::new("json")
+        ])
+    )]
+    pub format: String,
+}
+#[derive(Args, Debug, Clone)]
+pub struct FindArgs {
+    /// Type(s) (protocols) that need to be check on support by proxy
+    #[arg(long, required = true, num_args(1..),
+        value_parser([
+            PossibleValue::new("HTTP"),
+            PossibleValue::new("HTTPS"),
+            PossibleValue::new("SOCKS4"),
+        ]),
+    )]
+    pub types: Vec<String>,
+
+    /// Level(s) of anonymity (for HTTP only). By default, any level
+    #[arg(long, num_args(1..),
+        value_parser([
+            PossibleValue::new("Transparent"),
+            PossibleValue::new("Anonymous"),
+            PossibleValue::new("High")
+        ])
+    )]
+    pub levels: Vec<String>,
+
+    /// List of ISO country codes where should be located proxies
+    #[arg(short, long, num_args(1..))]
+    pub countries: Vec<String>,
+
+    /// The maximum number of working proxies
+    #[arg(short, long)]
+    pub limit: Option<usize>,
+
+    /// Flag indicating in what format the results will be presented.
+    #[arg(short, long,
+        default_value = "default",
+        value_parser([
+            PossibleValue::new("default"),
+            PossibleValue::new("text"),
+            PossibleValue::new("json")
+        ])
+    )]
+    pub format: String,
 }

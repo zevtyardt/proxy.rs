9bc97cf2780fe850a84ff12eeb6e70212527ad11
zevtyardt <apklover76@gmail.com>
2023-06-12 11:23:43 +0700

    refactor(providers): Rename module and file names
    - Rename git_repo module to github.
    - Rename proxyscrape_http module to http.
    - Rename proxyscrape_socks4 module to socks4.
    - Rename proxyscrape_socks5 module to socks5.
    - Rename proxyscan_http module to http.
    - Rename proxyscan_https module to https.
    - Rename proxyscan_socks4 module to socks4.
    - Rename proxyscan_socks5 module to socks5.
    - Add new providers for TheSpeedX proxy lists (http, socks4, socks5).

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -1,6 +1,6 @@
 pub mod base_provider;
 pub mod freeproxylist;
-pub mod git_repo;
+pub mod github;
 pub mod ipaddress_com;
 pub mod proxyscan;
 pub mod proxyscrape;
@@ -35,48 +35,61 @@ pub fn get_all_tasks() -> Vec<JoinHandle<()>> {
 
     /* ===== */
     tasks.push(spawn(async {
-        let mut proxyscrape_http =
-            proxyscrape::proxyscrape_http::ProxyscrapeComHttpProvider::default();
+        let mut proxyscrape_http = proxyscrape::http::ProxyscrapeComHttpProvider::default();
         proxyscrape_http.get_proxies().await;
     }));
     tasks.push(spawn(async {
-        let mut proxyscrape_socks4 =
-            proxyscrape::proxyscrape_socks4::ProxyscrapeComSocks4Provider::default();
+        let mut proxyscrape_socks4 = proxyscrape::socks4::ProxyscrapeComSocks4Provider::default();
         proxyscrape_socks4.get_proxies().await;
     }));
     tasks.push(spawn(async {
-        let mut proxyscrape_socks5 =
-            proxyscrape::proxyscrape_socks5::ProxyscrapeComSocks5Provider::default();
+        let mut proxyscrape_socks5 = proxyscrape::socks5::ProxyscrapeComSocks5Provider::default();
         proxyscrape_socks5.get_proxies().await;
     }));
 
     /* ===== */
     tasks.push(spawn(async {
         let mut zevtyardt_proxy_list =
-            git_repo::zevtyardt_proxy_list::GithubZevtyardtProxyListProvider::default();
+            github::zevtyardt_proxy_list::GithubZevtyardtProxyListProvider::default();
         zevtyardt_proxy_list.get_proxies().await;
+    }));
+
+    tasks.push(spawn(async {
+        let mut thespeedx_http_list =
+            github::thespeedx_socks_list::http::GithubTheSpeedXProxyListHttpProvider::default();
+        thespeedx_http_list.get_proxies().await;
+    }));
+
+    tasks.push(spawn(async {
+        let mut thespeedx_socks4_list =
+            github::thespeedx_socks_list::socks4::GithubTheSpeedXProxyListSocks4Provider::default();
+        thespeedx_socks4_list.get_proxies().await;
+    }));
+
+    tasks.push(spawn(async {
+        let mut thespeedx_socks5_list =
+            github::thespeedx_socks_list::socks5::GithubTheSpeedXProxyListSocks5Provider::default();
+        thespeedx_socks5_list.get_proxies().await;
     }));
 
     /* ===== */
     tasks.push(spawn(async {
-        let mut proxyscan_http = proxyscan::proxyscan_http::ProxyscanIoHttpProvider::default();
+        let mut proxyscan_http = proxyscan::http::ProxyscanIoHttpProvider::default();
         proxyscan_http.get_proxies().await;
     }));
 
     tasks.push(spawn(async {
-        let mut proxyscan_https = proxyscan::proxyscan_https::ProxyscanIoHttpsProvider::default();
+        let mut proxyscan_https = proxyscan::https::ProxyscanIoHttpsProvider::default();
         proxyscan_https.get_proxies().await;
     }));
 
     tasks.push(spawn(async {
-        let mut proxyscan_socks4 =
-            proxyscan::proxyscan_socks4::ProxyscanIoSocks4Provider::default();
+        let mut proxyscan_socks4 = proxyscan::socks4::ProxyscanIoSocks4Provider::default();
         proxyscan_socks4.get_proxies().await;
     }));
 
     tasks.push(spawn(async {
-        let mut proxyscan_socks5 =
-            proxyscan::proxyscan_socks5::ProxyscanIoSocks5Provider::default();
+        let mut proxyscan_socks5 = proxyscan::socks5::ProxyscanIoSocks5Provider::default();
         proxyscan_socks5.get_proxies().await;
     }));
 

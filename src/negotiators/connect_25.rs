740165c0675bbfb44bbbdf52729ef5dd8803aa72
zevtyardt <apklover76@gmail.com>
2023-06-13 21:15:00 +0700

    feat(negotiators): Add Connect25Negotiator and Connect80Negotiator

---
+++ src/negotiators/connect_25.rs
@@ -0,0 +1,43 @@
+use crate::{judge::Judge, proxy::Proxy, utils::http::Response};
+
+#[derive(Debug, Clone)]
+pub struct Connect25Negotiator {
+    pub name: String,
+    pub check_anon_lvl: bool,
+    pub use_full_path: bool,
+}
+
+impl Connect25Negotiator {
+    pub async fn negotiate(&self, proxy: &mut Proxy, judge: &Judge) -> bool {
+        let connect_payload = format!(
+            "CONNECT {0}:25 HTTP/1.1\r\nHost: {0}\r\nConnection: keep-alive\r\n\r\n",
+            judge.host
+        );
+        proxy.send(connect_payload.as_bytes()).await;
+
+        if let Some(data) = proxy.recv_all().await {
+            let response = Response::parse(data.as_slice());
+            if let Some(status_code) = response.status_code {
+                if status_code == 200 {
+                    return true;
+                }
+                proxy.log(
+                    format!("Connect: failed. HTTP status: {}", status_code).as_str(),
+                    None,
+                    Some("bad_status_error".to_string()),
+                );
+            }
+        }
+        false
+    }
+}
+
+impl Default for Connect25Negotiator {
+    fn default() -> Self {
+        Self {
+            name: "connect:25".to_string(),
+            check_anon_lvl: false,
+            use_full_path: false,
+        }
+    }
+}

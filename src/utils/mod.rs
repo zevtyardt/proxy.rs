5507f790ee99bac0dd1893b9a656bb1745f69622
zevtyardt <apklover76@gmail.com>
2023-06-14 18:06:01 +0700

    feat: Add version check functionality
    - Created a new file src/utils/update.rs
    - Added function check_version() to perform version check

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,7 @@
 pub mod geolite_database;
 pub mod http;
 pub mod serializer;
+pub mod update;
 
 macro_rules! vec_of_strings {
     ($($x:expr),*) => (vec![$($x.to_string()),*]);
---
+++ src/utils/update.rs
@@ -0,0 +1,16 @@
+const GITHUB_CARGO_URL: &str =
+    "https://raw.githubusercontent.com/zevtyardt/proxy.rs/main/Cargo.toml";
+
+pub async fn check_version() {
+    if let Ok(response) = reqwest::get(GITHUB_CARGO_URL).await {
+        if let Ok(text) = response.text().await {
+            let re = regex::Regex::new(r#"version = \"([\d.]+)\""#).unwrap();
+            if let Some(cap) = re.captures(text.as_str()) {
+                let latest_version = cap.get(1).unwrap().as_str();
+                let current_version = env!("CARGO_PKG_VERSION");
+
+                log::warn!("Version Mismatch:\nLatest version detected: v{}\nCurrent version: v{}\n\nPlease update or reinstall for compatibility. For more information:\nvisit https://github.com/zevtyardt/proxy.rs\n", latest_version, current_version);
+            }
+        }
+    }
+}

7e0f40ed5ecba8a398286c4f98a1e4b1cfa5cc4c
zevtyardt <apklover76@gmail.com>
2023-07-07 19:57:58 +0700

    refactor(main): Refactor main function to handle grab and serve commands separately
    - Move grab command handling to a separate function
    - Move find command handling to a separate function
    - Add support for serve command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![allow(dead_code)]
 //#![allow(unused_variables)]
-#![allow(unused_imports)]
+//#![allow(unused_imports)]
 //#![allow(unreachable_code)]
 
-use argument::{FindArgs, GrabArgs};
+use argument::GrabArgs;
 use checker::Checker;
 use clap::Parser;
 use futures_util::{stream, StreamExt};
@@ -11,20 +11,22 @@ use lazy_static::lazy_static;
 use parking_lot::Mutex;
 use proxy::Proxy;
 use regex::Regex;
+use server::{proxy_pool::LIVE_PROXIES, Server};
 use simple_logger::SimpleLogger;
-use std::{path::PathBuf, pin::Pin, process, sync::Arc, time::Duration};
+use std::{path::PathBuf, pin::Pin, sync::Arc, time::Duration};
 use tokio::{
     fs::File,
     io::{stdout, AsyncBufReadExt, AsyncWrite, AsyncWriteExt, BufReader},
     runtime,
-    sync::mpsc::{self, UnboundedSender},
-    task, time,
+    sync::mpsc::{self, Sender},
+    task,
+    time::{self, interval},
 };
 
 use crate::{
     argument::{Cli, Commands},
     providers::PROXIES,
-    utils::{run_parallel, update::check_version},
+    utils::update::check_version,
 };
 
 mod argument;
@@ -34,52 +36,33 @@ mod negotiators;
 mod providers;
 mod proxy;
 mod resolver;
+mod server;
 mod utils;
 
 lazy_static! {
     static ref STOP_FIND_LOOP: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
 }
 
-const EOF_MSG: &str = "==EOF==";
-
-async fn handle_grab_command(args: GrabArgs, tx: UnboundedSender<String>) {
-    let format = args.format;
+async fn handle_grab_command(args: GrabArgs, tx: Sender<Option<Proxy>>) {
     let expected_countries = args.countries;
 
     loop {
-        while let Ok((host, port, expected_types)) = PROXIES.pop() {
+        if let Ok((host, port, expected_types)) = PROXIES.pop() {
             if let Some(proxy) = proxy::Proxy::create(host.as_str(), port, expected_types).await {
                 if !expected_countries.is_empty()
                     && !expected_countries.contains(&proxy.geo.iso_code)
                 {
                     continue;
                 }
-
-                let msg = match format.as_str() {
-                    "text" => proxy.as_text(),
-                    "json" => proxy.as_json(),
-                    _ => format!("{}", proxy),
-                };
-
-                tx.send(msg).unwrap()
+                if tx.send(Some(proxy)).await.is_err() {
+                    return;
+                }
             }
         }
     }
 }
 
-async fn handle_find_command(
-    mut checker: Checker,
-    args: FindArgs,
-    max_conn: usize,
-    tx: UnboundedSender<String>,
-) {
-    // config
-    let format = args.format;
-
-    checker.expected_types = args.types;
-    checker.expected_levels = args.levels;
-    checker.expected_countries = args.countries;
-
+async fn handle_find_command(checker: Checker, max_conn: usize, tx: Sender<Option<Proxy>>) {
     while !*STOP_FIND_LOOP.lock() {
         let mut proxies = Vec::new();
         while let Ok((host, port, expected_types)) = PROXIES.pop() {
@@ -93,16 +76,10 @@ async fn handle_find_command(
             let ret = stream::iter(proxies)
                 .map(|mut proxy| {
                     let mut checker = checker.clone();
-                    let format = format.clone();
                     let tx = tx.clone();
                     task::spawn(async move {
                         if checker.check_proxy(&mut proxy).await {
-                            let msg = match format.as_str() {
-                                "text" => proxy.as_text(),
-                                "json" => proxy.as_json(),
-                                _ => format!("{}", proxy),
-                            };
-                            tx.send(msg).unwrap();
+                            tx.send(Some(proxy)).await.unwrap();
                         }
                     })
                 })
@@ -118,7 +95,7 @@ async fn handle_find_command(
             );
 
             if *STOP_FIND_LOOP.lock() {
-                tx.send(EOF_MSG.to_string()).unwrap()
+                tx.send(None).await.unwrap();
             }
         }
     }
@@ -164,8 +141,6 @@ fn main() {
         .init()
         .unwrap();
 
-    log::info!("Start collecting proxies.. ");
-
     runtime::Builder::new_multi_thread()
         .worker_threads(4)
         .enable_all()
@@ -174,18 +149,23 @@ fn main() {
         .block_on(async move {
             let mut tasks = vec![];
 
-            let max_tries = cli.max_tries as i32;
             let max_conn = cli.max_conn;
             let timeout = cli.timeout as i32;
 
             let mut files = vec![];
-            let (tx, mut rx) = mpsc::unbounded_channel();
+            let (tx, mut rx) = mpsc::channel(50);
 
-            let outfile;
-            let limit;
-            let format;
+            let mut outfile = None;
+            let mut limit = 0;
+            let mut format = "default".to_string();
 
-            tasks.push(task::spawn(check_version()));
+            let mut is_server = false;
+            let mut host = "127.0.0.1".to_string();
+            let mut port = 8080;
+
+            if !cli.skip_version_check {
+                tasks.push(task::spawn(check_version()));
+            }
 
             match cli.sub {
                 Commands::Grab(grab_args) => {
@@ -202,10 +182,13 @@ fn main() {
                     format = find_args.format.clone();
 
                     let mut checker = Checker::new().await;
-                    checker.max_tries = max_tries;
+                    checker.max_tries = find_args.max_tries as i32;
                     checker.timeout = timeout;
                     checker.support_cookie = find_args.support_cookies;
                     checker.support_referer = find_args.support_referer;
+                    checker.expected_types = find_args.types.clone();
+                    checker.expected_levels = find_args.levels;
+                    checker.expected_countries = find_args.countries;
 
                     let ext_ip = checker.ext_ip.clone();
 
@@ -218,9 +201,36 @@ fn main() {
                     files.extend(find_args.files.clone());
 
                     let tx = tx.clone();
-                    tasks.push(task::spawn(handle_find_command(
-                        checker, find_args, max_conn, tx,
-                    )));
+                    tasks.push(task::spawn(handle_find_command(checker, max_conn, tx)));
+                }
+
+                Commands::Serve(serve_args) => {
+                    is_server = true;
+
+                    host = serve_args.host;
+                    port = serve_args.port;
+
+                    let mut checker = Checker::new().await;
+                    checker.max_tries = serve_args.max_tries as i32;
+                    checker.support_cookie = true;
+                    checker.support_referer = true;
+
+                    checker.expected_types = serve_args.types.clone();
+                    checker.expected_levels = serve_args.levels;
+                    checker.expected_countries = serve_args.countries;
+
+                    let ext_ip = checker.ext_ip.clone();
+
+                    let expected_types = serve_args.types.clone();
+                    let verify_ssl = false;
+                    tasks.push(task::spawn(async move {
+                        checker::check_judges(verify_ssl, ext_ip, expected_types).await;
+                    }));
+
+                    files.extend(serve_args.files.clone());
+
+                    let tx = tx.clone();
+                    tasks.push(task::spawn(handle_find_command(checker, max_conn, tx)));
                 }
             }
 
@@ -231,49 +241,77 @@ fn main() {
                     *stop_file_loop = true
                 }))
             } else {
+                if !is_server {
+                    log::info!("Start collecting proxies.. ");
+                }
+
                 /* providers */
                 tasks.push(tokio::task::spawn(async {
+                    let mut interval = interval(Duration::from_secs(60));
                     loop {
-                        let all_providers = providers::get_all_tasks();
-                        run_parallel::<()>(all_providers, None).await;
-                        time::sleep(Duration::from_secs(10)).await;
+                        interval.tick().await;
+                        tokio::task::spawn(providers::run_all_providers(2));
                     }
                 }));
             }
 
-            let mut output: Pin<Box<dyn AsyncWrite>> = if let Some(path) = outfile {
-                let file = File::create(path).await.unwrap();
-                Box::pin(file)
-            } else {
-                Box::pin(stdout())
-            };
+            if is_server {
+                tasks.push(tokio::task::spawn(async move {
+                    let server = Server::new(host.as_str(), port);
+                    server.start().await;
+                }));
 
-            let mut open_list = false;
-            let mut counter = limit;
-            while let Some(msg) = rx.recv().await {
-                let stop = msg == EOF_MSG || (limit != 0 && counter <= 1);
-
-                if msg != EOF_MSG {
-                    if format == "json" && !open_list {
-                        output.write_all(b"[").await.unwrap();
-                        open_list = true;
+                loop {
+                    if let Some(Some(proxy)) = rx.recv().await {
+                        while LIVE_PROXIES.is_full() {
+                            continue;
+                        }
+                        LIVE_PROXIES.push(proxy).unwrap();
                     }
-
-                    output.write_all(msg.as_bytes()).await.unwrap();
-                    if stop {
-                        output
-                            .write_all(if format == "json" { b"]" } else { b"" })
-                            .await
-                            .unwrap();
-                    } else if format == "json" {
-                        output.write_all(b",").await.unwrap();
-                    }
-                    output.write_all(b"\n").await.unwrap();
                 }
-                counter -= 1;
+            } else {
+                let mut output: Pin<Box<dyn AsyncWrite>> = if let Some(path) = outfile {
+                    let file = File::create(path).await.unwrap();
+                    Box::pin(file)
+                } else {
+                    Box::pin(stdout())
+                };
 
-                if stop {
-                    std::process::exit(0);
+                let mut open_list = false;
+                let mut counter = limit;
+
+                while let Some(proxy) = rx.recv().await {
+                    let stop = proxy.is_none() || (limit != 0 && counter <= 1);
+                    if let Some(proxy) = proxy {
+                        if format == "json" && !open_list {
+                            output.write_all(b"[").await.unwrap();
+                            open_list = true;
+                        }
+
+                        let msg = match format.as_str() {
+                            "text" => proxy.as_text(),
+                            "json" => proxy.as_json(),
+                            _ => format!("{}", proxy),
+                        };
+
+                        output.write_all(msg.as_bytes()).await.unwrap();
+                        if stop {
+                            output
+                                .write_all(if format == "json" { b"]" } else { b"" })
+                                .await
+                                .unwrap();
+                        } else if format == "json" {
+                            output.write_all(b",").await.unwrap();
+                        }
+                        output.write_all(b"\n").await.unwrap();
+                    }
+                    if limit != 0 {
+                        counter -= 1;
+                    }
+
+                    if stop {
+                        std::process::exit(0);
+                    }
                 }
             }
         });

3eb88b44a8935e656934947083571b95d61bd73c
zevtyardt <apklover76@gmail.com>
2023-11-09 21:44:56 +0700

    refactor(main): Update main function and logging setup
    - Updated main function to return `anyhow::Result<()>`.
    - Moved the logging setup to the `setup_logger` function in the `logger` module.
    - Used the `setup_logger` function in the main function to set up the logging configuration.
    - Updated the import statements accordingly.

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,23 @@
 
 use std::process::exit;
 
-use owo_colors::OwoColorize;
-
-use crate::geolite::{geolite_exists, lookup::geo_lookup};
+use crate::{
+    geolite::{downloader::download_geolite_file, geolite_exists, lookup::geo_lookup},
+    utils::logger::setup_logger,
+};
 
 mod geolite;
 mod proxies;
 mod utils;
 
-fn setup_logging() {
-    fern::Dispatch::new()
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "{} {} {}",
-                record.level().green(),
-                record.target(),
-                message
-            ))
-        })
-        .level(log::LevelFilter::Off)
-        .level_for("proxy_rs", log::LevelFilter::Info)
-        .chain(std::io::stdout())
-        .apply()
-        .unwrap();
-}
+fn main() -> anyhow::Result<()> {
+    setup_logger(Some(log::LevelFilter::Debug));
 
-fn main() {
-    setup_logging();
-    if geolite_exists() {
-        log::info!("db tidak ada");
-        exit(0);
+    if !geolite_exists() {
+        download_geolite_file()?;
     }
-
     let proxy = geo_lookup("114.142.169.20");
     log::info!("{:#?}", proxy);
+
+    Ok(())
 }

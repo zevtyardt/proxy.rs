aca9c443b3b48e505a4112734ec69551ff7a620d
zevtyardt <apklover76@gmail.com>
2023-11-12 18:29:49 +0700

    refactor(geolite): Update check_geolite_db function
    - Updated check_geolite_db function to use MY_IP.get().await for fetching the IP address.
    - Removed unnecessary code related to measuring elapsed time and calculating average.
    - Added error logging in case of any errors during geo_lookup.
    - Simplified the return statement.

--- src/geolite/mod.rs
+++ src/geolite/mod.rs
@@ -2,7 +2,7 @@ use std::fs::remove_dir_all;
 
 use anyhow::Context;
 
-use crate::{error_context, geolite::lookup::geo_lookup, resolver::ip::my_ip, utils::get_data_dir};
+use crate::{error_context, geolite::lookup::geo_lookup, resolver::ip::MY_IP, utils::get_data_dir};
 
 pub mod downloader;
 pub mod lookup;
@@ -42,24 +42,11 @@ pub async fn check_geolite_db() -> anyhow::Result<bool> {
         return Ok(false);
     }
 
-    // twst
-    let my_ip = my_ip().await.context(error_context!())?;
-
-    let mut averages = vec![];
-    for i in 0..1000 {
-        let instant = tokio::time::Instant::now();
-        let result = geo_lookup(my_ip).await.context(error_context!())?;
-        let e = instant.elapsed();
-        averages.push(e.clone());
-        log::info!("elapsed-{}: {:#?}", i + 1, e);
-        if i == 0 {
-            log::info!("{:#?}", result);
-        }
+    let my_ip = MY_IP.get().await;
+    if let Err(err) = geo_lookup(*my_ip).await {
+        log::error!("{:?}", err);
+        return Ok(false);
     }
-    let total = averages.len();
-    let sum: tokio::time::Duration = averages.iter().sum();
-    let average = sum / total as u32;
-    log::info!("average: {:#?}", average);
 
     Ok(true)
 }

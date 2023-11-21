e437b7f05ee8e6290af34de1510557e44ba37b44
zevtyardt <apklover76@gmail.com>
2023-11-21 10:01:18 +0700

    refactor(main): Refactor main.rs for improved readability and concurrency
    - Added imports for `Arc` and `FuturesUnordered` for concurrency.
    - Imported judge modules for HTTP, HTTPS, and SMTP.
    - Added a semaphore for concurrency control.
    - Refactored the initialization of HTTP and HTTPS judges to use `tokio::spawn`.
    - Modified the main function to use `FuturesUnordered` for concurrent execution of judge requests.
    - Implemented a loop to await the completion of all judge requests.
    - Improved logging for judge URLs.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+use std::sync::Arc;
+
 use anyhow::Context;
+use futures_util::{stream::FuturesUnordered, StreamExt};
+use judges::{
+    http::{get_http_judge, init_http_judge},
+    https::{get_https_judge, init_https_judge},
+    smtp::get_smtp_judge,
+};
+use tokio::sync::Semaphore;
 
 use crate::{
     geolite::{check_geolite_db, downloader::download_geolite_db},
@@ -10,6 +19,7 @@ use crate::{
 
 mod checkers;
 mod geolite;
+mod judges;
 mod proxy;
 mod resolver;
 mod utils;
@@ -21,6 +31,51 @@ fn start_app() -> anyhow::Result<()> {
             download_geolite_db().await.context(error_context!())?;
             return Ok(());
         }
+
+        tokio::spawn(async {
+            log::info!("http init");
+            (
+                "http",
+                init_http_judge()
+                    .await
+                    .context(error_context!())
+                    .unwrap_or(false),
+            )
+        });
+        tokio::spawn(async {
+            log::info!("https init");
+            (
+                "https",
+                init_https_judge()
+                    .await
+                    .context(error_context!())
+                    .unwrap_or(false),
+            )
+        });
+
+        log::info!("main");
+        let mut fut = FuturesUnordered::new();
+        let sem = Arc::new(Semaphore::new(500));
+
+        for i in 0..10 {
+            let permit = Arc::clone(&sem).acquire_owned().await;
+            fut.push(async move {
+                let _ = permit;
+                if let Ok(url) = get_smtp_judge().await.context(error_context!()) {
+                    log::info!("{:?}", url);
+                }
+                if let Ok(url) = get_http_judge().await.context(error_context!()) {
+                    log::info!("{:?}", url);
+                }
+                if let Ok(url) = get_https_judge().await.context(error_context!()) {
+                    log::info!("{:?}", url);
+                }
+            })
+        }
+        while (fut.next().await).is_some() {
+            continue;
+        }
+
         Ok(())
     })
 }
